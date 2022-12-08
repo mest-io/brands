ddfd449100d2f64e2a326be7fade97d7f2d5e73d
aquariuslt <superaquariuslt@gmail.com>
2022-12-09 07:50:07 +0800

    chore(deps): add `@mest-fe` eslint deps

--- apps/docs/src/app/app.spec.tsx
+++ apps/docs/src/app/app.spec.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import { render } from '@testing-library/react'
 
 import { BrowserRouter } from 'react-router-dom'
@@ -14,5 +15,4 @@ describe('App', () => {
 
     expect(baseElement).toBeTruthy()
   })
-
 })
