c344cf3d88bd1002c4e620c84d94c7b2fee3fc01
aquariuslt <superaquariuslt@gmail.com>
2022-12-08 13:31:34 +0800

    feat(brands): add svg to jsx build script with `svgo-jsx`

--- packages/brands/src/index.ts
+++ packages/brands/src/index.ts
@@ -1,1 +1,12 @@
-export * from './lib/brands'
+// @ts-ignore
+export { LogoCardHorizontalDark } from './components/LogoCardHorizontalDark'
+// @ts-ignore
+export { LogoCardHorzontalLight } from './components/LogoCardHorzontalLight'
+// @ts-ignore
+export { LogoHorizontalBlack } from './components/LogoHorizontalBlack'
+// @ts-ignore
+export { LogoHorizontalLight } from './components/LogoHorizontalLight'
+// @ts-ignore
+export { LogoVerticalDark } from './components/LogoVerticalDark'
+// @ts-ignore
+export { LogoVerticalLight } from './components/LogoVerticalLight'
