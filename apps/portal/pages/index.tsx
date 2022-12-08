7488d6c7a00ca40d1c8e898e7a18e4b12fdea415
aquariuslt <superaquariuslt@gmail.com>
2022-12-08 10:02:47 +0800

    chore(deps): generate next app `portal` for official site

---
+++ apps/portal/pages/index.tsx
@@ -0,0 +1,14 @@
+export function Index() {
+  /*
+   * Replace the elements below with your own.
+   *
+   * Note: The corresponding styles are in the ./index.styled-jsx file.
+   */
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default Index
