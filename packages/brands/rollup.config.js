6ff3b4934e3606e61586ca6e6bef6c4d99dbbe4a
aquariuslt <superaquariuslt@gmail.com>
2022-12-09 11:39:06 +0800

    chore: using svgr for react-component generating

--- packages/brands/rollup.config.js
+++ packages/brands/rollup.config.js
@@ -5,6 +5,7 @@ import terser from '@rollup/plugin-terser'
 import commonjs from '@rollup/plugin-commonjs'
 import copy from 'rollup-plugin-copy'
 import del from 'rollup-plugin-delete'
+import peerDepsExternal from 'rollup-plugin-peer-deps-external'
 
 import path from 'node:path'
 import fs from 'node:fs'
@@ -13,12 +14,49 @@ const root = path.join(__dirname, './')
 const entryPath = path.join(root, 'src', 'index.ts')
 const tsconfigPath = path.join(root, 'tsconfig.lib.json')
 const distPath = path.join(root, '../../', 'dist', 'packages', 'brands')
-
 const componentsPath = path.join(root, 'src', 'components')
 
+const globals = {
+  react: 'React',
+  'react-dom': 'ReactDOM',
+}
+const external = [
+  'react',
+  'react-dom',
+  'classname',
+  'react-is',
+  'core-js',
+  'object-assign',
+  '**/node_modules/**',
+]
 const extensions = ['.js', '.jsx', '.ts', '.tsx']
 
-const outputName = path.join(distPath, 'index')
+const babelOptions = {
+  babelHelpers: 'runtime',
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        useBuiltIns: 'usage',
+      },
+    ],
+    '@babel/preset-typescript',
+    '@babel/preset-react',
+  ],
+  extensions: extensions,
+  exclude: '**/node_modules/**',
+}
+
+const commonPlugins = [
+  peerDepsExternal(),
+  resolve(),
+  commonjs(),
+  typescript({
+    check: true,
+    tsconfig: tsconfigPath,
+  }),
+  babel(babelOptions),
+]
 
 const convertCopyAssetsToRollupOptions = (outputPath, assets) => {
   return assets
@@ -28,6 +66,10 @@ const convertCopyAssetsToRollupOptions = (outputPath, assets) => {
       }))
     : undefined
 }
+
+const outputName = path.join(distPath, 'index')
+
+
 
 export default [
   {
@@ -59,21 +101,7 @@ export default [
           },
         ]),
       }),
-      resolve(),
-      typescript({
-        check: true,
-        tsconfig: tsconfigPath,
-      }),
-      resolve({
-        preferBuiltins: true,
-        extensions,
-      }),
-      babel({
-        babelHelpers: 'runtime',
-        extensions,
-        exclude: '**/node_modules/**',
-      }),
-      commonjs(),
+      ...commonPlugins,
     ],
     input: entryPath,
     output: [
@@ -81,87 +109,65 @@ export default [
         file: `${outputName}.js`,
         format: 'cjs',
         sourcemap: true,
+        globals,
       },
       {
         file: `${outputName}.umd.js`,
         name: `${outputName}`,
         format: 'umd',
         sourcemap: true,
+        globals,
       },
       {
-        file: `${outputName}.es.js`,
-        format: 'es',
+        file: `${outputName}.esm.js`,
+        format: 'esm',
         sourcemap: true,
+        globals,
       },
     ],
+    external,
   },
   {
-    plugins: [
-      resolve(),
-      typescript({
-        check: true,
-        tsconfig: tsconfigPath,
-      }),
-      resolve({
-        preferBuiltins: true,
-        extensions,
-      }),
-      babel({
-        babelHelpers: 'runtime',
-        extensions,
-        exclude: '**/node_modules/**',
-      }),
-      commonjs(),
-      terser(),
-    ],
+    plugins: [...commonPlugins, terser()],
     input: entryPath,
     output: [
       {
         file: `${outputName}.min.js`,
         format: 'cjs',
         sourcemap: true,
+        globals,
       },
       {
         file: `${outputName}.umd.min.js`,
         name: `${outputName}`,
         format: 'umd',
         sourcemap: true,
+        globals,
       },
       {
-        file: `${outputName}.es.min.js`,
-        format: 'es',
+        file: `${outputName}.esm.min.js`,
+        format: 'esm',
         sourcemap: true,
+        globals,
       },
     ],
+    external,
   },
   // build for each component
   ...fs.readdirSync(componentsPath).map(componentFilePath => {
     const filePath = path.join(root, 'src', 'components', componentFilePath)
+    const componentName = path.basename(componentFilePath, '.tsx')
     return {
-      plugins: [
-        resolve(),
-        typescript({
-          check: true,
-          tsconfig: tsconfigPath,
-        }),
-        resolve({
-          preferBuiltins: true,
-          extensions,
-        }),
-        babel({
-          babelHelpers: 'runtime',
-          extensions,
-          exclude: '**/node_modules/**',
-        }),
-        commonjs(),
-      ],
+      plugins: [...commonPlugins],
       input: filePath,
       output: [
         {
-          file: `${path.join(distPath, 'esm', componentFilePath)}`,
-          format: 'cjs',
+          file: `${path.join(distPath, 'esm', componentName)}.js`,
+          format: 'esm',
+          globals,
         },
       ],
+      external,
     }
   }),
 ]
