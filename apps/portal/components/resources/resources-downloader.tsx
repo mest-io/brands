d036c3015775d4d8045de0cf0cf858c129cbb7ba
aquariuslt <superaquariuslt@gmail.com>
2022-12-09 15:56:22 +0800

    style: migrate base brand ui & layout from portal

---
+++ apps/portal/components/resources/resources-downloader.tsx
@@ -0,0 +1,94 @@
+import * as React from 'react'
+import { Button, Grid } from '@mest-fe/ui'
+import {
+  LogoHorizontalDark,
+  LogoHorizontalLight,
+  LogoVerticalDark,
+  LogoVerticalLight,
+} from '@mest-fe/brands'
+
+interface DownloadButtonProps {
+  url: string
+  download: string
+}
+
+const downloadFromUrl = (url: string, download: string) => {
+  let downloadHref = document.createElement('a')
+  downloadHref.href = url
+  downloadHref.download = download
+  downloadHref.click()
+}
+const DownloadButton: React.FC<DownloadButtonProps> = props => {
+  return (
+    <Button onClick={() => downloadFromUrl(props.url, props.download)}>
+      Download
+    </Button>
+  )
+}
+
+export const ResourcesDownloader: React.FC = () => {
+  return (
+    <>
+      <Grid container gap={4} justify={'center'}>
+        <Grid xs={12} sm={6}>
+          <LogoHorizontalLight
+            height={320}
+            viewBox={'0 0 1830 463'}
+            preserveAspectRatio="xMidYMid meet"
+          />
+        </Grid>
+        <Grid xs={12} sm={6}>
+          <LogoHorizontalDark
+            height={320}
+            viewBox={'0 0 1830 463'}
+            preserveAspectRatio="xMidYMid meet"
+          />
+        </Grid>
+        <Grid xs={12} sm={6}>
+          <LogoVerticalLight
+            height={320}
+            viewBox={'0 0 916 882'}
+            preserveAspectRatio="xMidYMid meet"
+          />
+        </Grid>
+        <Grid xs={12} sm={6}>
+          <LogoVerticalDark
+            height={320}
+            viewBox={'0 0 916 882'}
+            preserveAspectRatio="xMidYMid meet"
+          />
+        </Grid>
+
+        <Grid xs={12} sm={6} justify={'center'}>
+          <DownloadButton
+            url={'/assets/MestHorizontalLight.png'}
+            download={'MestHorizontalLight.png'}
+          />
+        </Grid>
+        <Grid xs={12} sm={6} justify={'center'}>
+          <DownloadButton
+            url={'/assets/MestHorizontalDark.png'}
+            download={'MestHorizontalDark.png'}
+          />
+        </Grid>
+        <Grid xs={12} sm={6} justify={'center'}>
+          <DownloadButton
+            url={'/assets/MestVerticalLight.png'}
+            download={'MestVerticalLight.png'}
+          />
+        </Grid>
+        <Grid xs={12} sm={6} justify={'center'}>
+          <DownloadButton
+            url={'/assets/MestVerticalDark.png'}
+            download={'MestVerticalDark.png'}
+          />
+        </Grid>
+      </Grid>
+      <style jsx>{`
+        .componentCard:hover {
+          background-color: #191919;
+        }
+      `}</style>
+    </>
+  )
+}
--- apps/portal/pages/index.tsx
+++ apps/portal/pages/index.tsx
@@ -1,6 +1,145 @@
+import { Box, Grid, Image, ImageProps, Text } from '@mest-fe/ui'
+import * as React from 'react'
+import { LogoCardHorizontalLight } from '@mest-fe/brands'
+import { ResourcesDownloader } from '../components/resources/resources-downloader'
+
+export const CardTitle: React.FC<React.PropsWithChildren> = ({ children }) => {
+  return (
+    <Text
+      h2
+      css={{
+        color: '$accents_9',
+        fontSize: '3.5rem',
+        lineHeight: '1.5',
+        fontWeight: '700',
+
+        '@xs': {
+          fontSize: '2.25rem',
+          fontWeight: '600',
+        },
+        '@sm': {
+          fontSize: '2.5rem',
+          fontWeight: '600',
+        },
+        '@md': {
+          fontSize: '3rem',
+          fontWeight: '600',
+        },
+      }}>
+      {children}
+    </Text>
+  )
+}
+
+export const CardText: React.FC<React.PropsWithChildren> = ({ children }) => {
+  return (
+    <Text
+      css={{
+        color: '$accents_8',
+        fontSize: '1.5rem',
+        lineHeight: '1.5',
+
+        '@xs': {
+          fontSize: '1rem',
+        },
+        '@sm': {
+          fontSize: '1.1rem',
+        },
+        '@md': {
+          fontSize: '1.25rem',
+        },
+      }}>
+      {children}
+    </Text>
+  )
+}
+
+export const CardTextBox: React.FC<React.PropsWithChildren> = ({ children }) => {
+  return (
+    <Box
+      css={{
+        padding: '100px 100px',
+
+        '@xs': {
+          padding: '17px 20px 25px',
+        },
+        '@sm': {
+          padding: '80px 10px 20px 40px',
+        },
+        '@md': {
+          padding: '80px 15px 20px 40px',
+        },
+      }}>
+      {children}
+    </Box>
+  )
+}
+
+export const CardImage: React.FC<ImageProps> = ({ src }) => {
+  return (
+    <Grid
+      css={{
+        '@xs': {
+          height: 'auto',
+          padding: '40px 0 10px',
+        },
+      }}>
+      <Image
+        height="340px"
+        width="340px"
+        src={src}
+        alt="Mest features"
+        draggable={false}
+        css={{
+          userSelect: 'none',
+
+          '@xs': {
+            maxWidth: '75%',
+            height: 'auto !important',
+            img: {
+              height: 'auto',
+            },
+          },
+          '@sm': {
+            maxWidth: '70%',
+          },
+          '@md': {
+            maxWidth: '80%',
+          },
+        }}
+      />
+    </Grid>
+  )
+}
 
 export const Index = ({ pageProps }) => {
-  return <div></div>
+  return (
+    <>
+      <Grid
+        container
+        css={{
+          '@xs': {
+            flexDirection: 'column-reverse',
+          },
+        }}>
+        <Grid xs={24} sm={12}>
+          <CardTextBox>
+            <CardTitle>{'Mest Brands'}</CardTitle>
+            <CardText>{'Introducing Mest Branding Resources'}</CardText>
+          </CardTextBox>
+        </Grid>
+        <Grid xs={24} sm={12} justify="center" alignItems="center">
+          <LogoCardHorizontalLight
+            preserveAspectRatio="xMidYMid meet"
+            width="440"
+            height="340"
+            viewBox={'0 0 2500 1085'}
+          />
+        </Grid>
+      </Grid>
+      <ResourcesDownloader />
+    </>
+  )
 }
 
 export default Index
