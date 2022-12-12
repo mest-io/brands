c94b6c00c94943af19d332571907bef9f18db771
aquariuslt <superaquariuslt@gmail.com>
2022-12-12 10:04:21 +0800

    styles: duplicate ui blocks from `https://mest.io/team/media`

--- apps/portal/pages/index.tsx
+++ apps/portal/pages/index.tsx
@@ -2,6 +2,11 @@ import { Box, Grid, Image, ImageProps, Text } from '@mest-fe/ui'
 import * as React from 'react'
 import { LogoCardHorizontalLight } from '@mest-fe/brands'
 import { ResourcesDownloader } from '../components/resources/resources-downloader'
+import { AppContainer } from '../components/layouts/app/app-container/app-container'
+import { AppContainerSection } from '../components/layouts/app/app-container/app-container-section'
+import { Banner } from '../components/kits/banner'
+import { MediaText } from '../components/media/media-text'
+import { MediaTextBox } from '../components/media/media-text-box'
 
 export const CardTitle: React.FC<React.PropsWithChildren> = ({ children }) => {
   return (
@@ -115,28 +120,71 @@ export const CardImage: React.FC<ImageProps> = ({ src }) => {
 export const Index = ({ pageProps }) => {
   return (
     <>
-      <Grid
-        container
-        css={{
-          '@xs': {
-            flexDirection: 'column-reverse',
-          },
-        }}>
-        <Grid xs={24} sm={12}>
-          <CardTextBox>
-            <CardTitle>{'Mest Brands'}</CardTitle>
-            <CardText>{'Introducing Mest Branding Resources'}</CardText>
-          </CardTextBox>
+      <AppContainerSection>
+        <Banner
+          label={'Media Kit'}
+          title={'Brand Values'}
+          subtitle={'Open, Composable and Connectable'}
+        />
+      </AppContainerSection>
+      <AppContainerSection>
+        <Grid
+          container
+          css={{
+            padding: '65px 0',
+            flexDirection: 'row',
+            '@sm': {
+              padding: '35px 0',
+            },
+            '@xs': {
+              padding: '35px 0',
+              flexDirection: 'column-reverse',
+            },
+          }}>
+          <Grid xs={24} sm={12} md={10} direction="column">
+            <MediaTextBox>
+              <Text
+                h2
+                css={{
+                  fontWeight: 'bold',
+                  fontSize: '3.5rem',
+                  lineHeight: 1.25,
+                  color: '$accents_9',
+
+                  '@md': {
+                    fontSize: '3rem',
+                  },
+                  '@sm': {
+                    fontSize: '2.5rem',
+                  },
+                  '@xs': {
+                    fontSize: '2rem',
+                  },
+                }}>
+                {'Media Kit'}
+              </Text>
+              <MediaText>
+                {
+                  'Mest will provide you with best practice examples for the media kit and various types of logos.'
+                }
+              </MediaText>
+            </MediaTextBox>
+          </Grid>
+          <Grid xs={24} sm={12} md={14}>
+            <Image
+              draggable={false}
+              width="695px"
+              height="248px"
+              alt="Large logo variants"
+              src="/assets/logo-variants-media-large.png"
+              css={{ maxWidth: '95%', userSelect: 'none' }}
+            />
+          </Grid>
         </Grid>
-        <Grid xs={24} sm={12} justify="center" alignItems="center">
-          <LogoCardHorizontalLight
-            preserveAspectRatio="xMidYMid meet"
-            width="440"
-            height="340"
-          />
-        </Grid>
-      </Grid>
-      <ResourcesDownloader />
+      </AppContainerSection>
+      <AppContainerSection>
+        <ResourcesDownloader />
+      </AppContainerSection>
     </>
   )
 }
